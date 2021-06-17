6346616fd22883f99fd0b0b9db4420e2bd19b394
Stereowalker <stereowalker.mc@gmail.com>
2021-06-17 04:22:50 +0100

    Prevented food from reducing due to player movement when stamina is enabled

---
+++ src/main/java/com/stereowalker/survive/mixins/PlayerEntityMixin.java
@@ -0,0 +1,27 @@
+package com.stereowalker.survive.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.stereowalker.survive.config.Config;
+import com.stereowalker.survive.entity.SurviveEntityStats;
+import com.stereowalker.survive.util.EnergyStats;
+
+import net.minecraft.entity.player.PlayerEntity;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addExhaustion(F)V"), method = {"jump", "damageEntity", "attackTargetEntityWithCurrentItem", "addMovementStat"})
+	public void staminaExhaustion(PlayerEntity player, float value) {
+		if (Config.enable_stamina) {
+			EnergyStats energyStats = SurviveEntityStats.getEnergyStats(player);
+			energyStats.addExhaustion(player, value);
+			SurviveEntityStats.setEnergyStats(player, energyStats);
+		}
+		else {
+			player.addExhaustion(value);
+		}
+	}
+}
