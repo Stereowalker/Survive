daa84949e99a9b0c1b3405f2da5e1feaee40ff48
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2021-08-25 10:28:06 +0100

    Added a minimum version for mixin

--- src/main/java/com/stereowalker/survive/potion/UnwellEffect.java
+++ src/main/java/com/stereowalker/survive/potion/UnwellEffect.java
@@ -3,21 +3,9 @@ package com.stereowalker.survive.potion;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.stereowalker.survive.config.Config;
-import com.stereowalker.survive.entity.SurviveEntityStats;
-import com.stereowalker.survive.util.StaminaStats;
-import com.stereowalker.survive.util.SDamageSource;
-import com.stereowalker.survive.util.TemperatureStats;
-import com.stereowalker.survive.util.WaterStats;
-
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
-import net.minecraft.world.Difficulty;
 
 public class UnwellEffect extends Effect{
 
