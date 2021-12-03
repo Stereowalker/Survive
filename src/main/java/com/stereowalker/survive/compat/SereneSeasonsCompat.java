062f16653e32e36488754a6f60de586516194246
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2021-12-04 00:43:42 +0100

    Temporarily disabled serene seasons compatability

--- src/main/java/com/stereowalker/survive/compat/SereneSeasonsCompat.java
+++ src/main/java/com/stereowalker/survive/compat/SereneSeasonsCompat.java
@@ -10,92 +10,92 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
-import sereneseasons.api.season.SeasonHelper;
-import sereneseasons.config.BiomeConfig;
+//import sereneseasons.api.season.SeasonHelper;
+//import sereneseasons.config.BiomeConfig;
 
 public class SereneSeasonsCompat {
 
 	public static boolean snowsHere(Level world, BlockPos pos) {
-		if (world.getBiomeName(pos).isPresent() && !sereneseasons.config.BiomeConfig.usesTropicalSeasons(world.getBiomeName(pos).get())) {
-			return SeasonHelper.getSeasonState(world).getSeason() == sereneseasons.api.season.Season.WINTER;
-		}
+//		if (world.getBiomeName(pos).isPresent() && !sereneseasons.config.BiomeConfig.usesTropicalSeasons(world.getBiomeName(pos).get())) {
+//			return SeasonHelper.getSeasonState(world).getSeason() == sereneseasons.api.season.Season.WINTER;
+//		}
 		return false;
 	}
 
 	public static Season modifyTemperatureBySeason(Level world, BlockPos pos) {
-		if (world.getBiomeName(pos) != null &&
-				world.getBiomeName(pos).isPresent()) {
-			if (sereneseasons.config.BiomeConfig.usesTropicalSeasons(world.getBiomeName(pos).get())) {
-				switch (SeasonHelper.getSeasonState(world).getTropicalSeason()) {
-				case EARLY_DRY: return Seasons.DRY_BEGIN;
-				case MID_DRY: return Seasons.DRY_MIDST;
-				case LATE_DRY: return Seasons.DRY_CLOSE;
-
-				case EARLY_WET: return Seasons.WET_BEGIN;
-				case MID_WET: return Seasons.WET_MIDST;
-				case LATE_WET: return Seasons.WET_CLOSE;
-				}
-			}
-			else {
-				switch (SeasonHelper.getSeasonState(world).getSubSeason()) {
-				case EARLY_WINTER: return Seasons.WINTER_BEGIN;
-				case MID_WINTER: return Seasons.WINTER_MIDST;
-				case LATE_WINTER: return Seasons.WINTER_CLOSE;
-
-				case EARLY_SPRING: return Seasons.SPRING_BEGIN;
-				case MID_SPRING: return Seasons.SPRING_MIDST;
-				case LATE_SPRING: return Seasons.SPRING_CLOSE;
-
-				case EARLY_SUMMER: return Seasons.SUMMER_BEGIN;
-				case MID_SUMMER: return Seasons.SUMMER_MIDST;
-				case LATE_SUMMER: return Seasons.SUMMER_CLOSE;
-
-				case EARLY_AUTUMN: return Seasons.AUTUMN_BEGIN;
-				case MID_AUTUMN: return Seasons.AUTUMN_MIDST;
-				case LATE_AUTUMN: return Seasons.AUTUMN_CLOSE;
-				}
-			}
-		}
+//		if (world.getBiomeName(pos) != null &&
+//				world.getBiomeName(pos).isPresent()) {
+//			if (sereneseasons.config.BiomeConfig.usesTropicalSeasons(world.getBiomeName(pos).get())) {
+//				switch (SeasonHelper.getSeasonState(world).getTropicalSeason()) {
+//				case EARLY_DRY: return Seasons.DRY_BEGIN;
+//				case MID_DRY: return Seasons.DRY_MIDST;
+//				case LATE_DRY: return Seasons.DRY_CLOSE;
+//
+//				case EARLY_WET: return Seasons.WET_BEGIN;
+//				case MID_WET: return Seasons.WET_MIDST;
+//				case LATE_WET: return Seasons.WET_CLOSE;
+//				}
+//			}
+//			else {
+//				switch (SeasonHelper.getSeasonState(world).getSubSeason()) {
+//				case EARLY_WINTER: return Seasons.WINTER_BEGIN;
+//				case MID_WINTER: return Seasons.WINTER_MIDST;
+//				case LATE_WINTER: return Seasons.WINTER_CLOSE;
+//
+//				case EARLY_SPRING: return Seasons.SPRING_BEGIN;
+//				case MID_SPRING: return Seasons.SPRING_MIDST;
+//				case LATE_SPRING: return Seasons.SPRING_CLOSE;
+//
+//				case EARLY_SUMMER: return Seasons.SUMMER_BEGIN;
+//				case MID_SUMMER: return Seasons.SUMMER_MIDST;
+//				case LATE_SUMMER: return Seasons.SUMMER_CLOSE;
+//
+//				case EARLY_AUTUMN: return Seasons.AUTUMN_BEGIN;
+//				case MID_AUTUMN: return Seasons.AUTUMN_MIDST;
+//				case LATE_AUTUMN: return Seasons.AUTUMN_CLOSE;
+//				}
+//			}
+//		}
 		return Seasons.NONE;
 	}
 	
 	
-    /**
-     * From Serene Seasons, Altered the temp
-     * @param subSeason
-     * @param biome
-     * @param key
-     * @param pos
-     * @return
-     */
-    public static float getBiomeTemperatureInSeason(sereneseasons.api.season.Season.SubSeason subSeason, Biome biome, ResourceKey<Biome> key, BlockPos pos)
-    {
-        boolean tropicalBiome = BiomeConfig.usesTropicalSeasons(key);
-        float biomeTemp = TemperatureUtil.getTemperature(biome, pos);
-        if (!tropicalBiome && biome.getBaseTemperature() <= 0.8F && BiomeConfig.enablesSeasonalEffects(key))
-        {
-            switch (subSeason)
-            {
-                default:
-                    break;
-
-                case LATE_SPRING: case EARLY_AUTUMN:
-                biomeTemp = Mth.clamp(biomeTemp - 0.1F, -0.5F, 2.0F);
-                break;
-
-                case MID_SPRING: case MID_AUTUMN:
-                biomeTemp = Mth.clamp(biomeTemp - 0.2F, -0.5F, 2.0F);
-                break;
-
-                case EARLY_SPRING: case LATE_AUTUMN:
-                biomeTemp = Mth.clamp(biomeTemp - 0.4F, -0.5F, 2.0F);
-                break;
-
-                case EARLY_WINTER: case MID_WINTER: case LATE_WINTER:
-                biomeTemp = Mth.clamp(biomeTemp - 0.8F, -0.5F, 2.0F);
-                break;
-            }
-        }
-        return biomeTemp;
-    }
+//    /**
+//     * From Serene Seasons, Altered the temp
+//     * @param subSeason
+//     * @param biome
+//     * @param key
+//     * @param pos
+//     * @return
+//     */
+//    public static float getBiomeTemperatureInSeason(sereneseasons.api.season.Season.SubSeason subSeason, Biome biome, ResourceKey<Biome> key, BlockPos pos)
+//    {
+//        boolean tropicalBiome = BiomeConfig.usesTropicalSeasons(key);
+//        float biomeTemp = TemperatureUtil.getTemperature(biome, pos);
+//        if (!tropicalBiome && biome.getBaseTemperature() <= 0.8F && BiomeConfig.enablesSeasonalEffects(key))
+//        {
+//            switch (subSeason)
+//            {
+//                default:
+//                    break;
+//
+//                case LATE_SPRING: case EARLY_AUTUMN:
+//                biomeTemp = Mth.clamp(biomeTemp - 0.1F, -0.5F, 2.0F);
+//                break;
+//
+//                case MID_SPRING: case MID_AUTUMN:
+//                biomeTemp = Mth.clamp(biomeTemp - 0.2F, -0.5F, 2.0F);
+//                break;
+//
+//                case EARLY_SPRING: case LATE_AUTUMN:
+//                biomeTemp = Mth.clamp(biomeTemp - 0.4F, -0.5F, 2.0F);
+//                break;
+//
+//                case EARLY_WINTER: case MID_WINTER: case LATE_WINTER:
+//                biomeTemp = Mth.clamp(biomeTemp - 0.8F, -0.5F, 2.0F);
+//                break;
+//            }
+//        }
+//        return biomeTemp;
+//    }
 }
