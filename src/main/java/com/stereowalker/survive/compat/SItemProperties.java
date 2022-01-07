5eaea45cf686111923037b6b046ac5e4ba52036d
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2022-01-07 02:30:27 +0100

    Fix thermometer not working on the hotbar and item frame

--- src/main/java/com/stereowalker/survive/compat/SItemProperties.java
+++ src/main/java/com/stereowalker/survive/compat/SItemProperties.java
@@ -7,6 +7,7 @@ import java.util.Map.Entry;
 import com.stereowalker.survive.world.item.SItems;
 import com.stereowalker.survive.world.temperature.TemperatureQuery;
 
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Tuple;
@@ -17,42 +18,46 @@ public class SItemProperties {
 		ItemProperties.register(SItems.THERMOMETER, new ResourceLocation("temperature"), (stack, level, livingentity, count) -> {
 			Entity entity = (Entity)(livingentity != null ? livingentity : stack.getEntityRepresentation());
 			if (entity == null) {
-				return 0.0f;
-			}
-			else if (level == null && entity.level == null) {
-				return 0.0f;
-			}
-			float environmentalMods = 0;
-			for (Entry<ResourceLocation, Tuple<TemperatureQuery, ContributingFactor>> query : TemperatureQuery.queries.entrySet()) {
-				if (query.getValue().getB() == ContributingFactor.ENVIRONMENTAL ) {
-					environmentalMods += query.getValue().getA().run(null, 0, level == null ? level : entity.level, entity.blockPosition());
-//					System.out.println(environmentalMods+" "+query.getKey());
+				return 0.0F;
+			} else {
+				if (level == null && entity.level instanceof ClientLevel) {
+					level = (ClientLevel)entity.level;
+				}
+
+				if (level == null) {
+					return 0.0F;
+				} else {
+					float environmentalMods = 0;
+					for (Entry<ResourceLocation, Tuple<TemperatureQuery, ContributingFactor>> query : TemperatureQuery.queries.entrySet()) {
+						if (query.getValue().getB() == ContributingFactor.ENVIRONMENTAL ) {
+							environmentalMods += query.getValue().getA().run(null, 0, level, entity.blockPosition());
+						}
+					}
+					float val = 0.0f;
+					if (environmentalMods >= 2.0f) {
+						val = 1.0f;
+					} else if (environmentalMods >= 1.6f) {
+						val = 0.9f;
+					} else if (environmentalMods >= 1.2f) {
+						val = 0.8f;
+					} else if (environmentalMods >= 0.8f) {
+						val = 0.7f;
+					} else if (environmentalMods >= 0.4f) {
+						val = 0.6f;
+					} else if (environmentalMods >= 0.0f) {
+						val = 0.5f;
+					} else if (environmentalMods >= -0.4f) {
+						val = 0.4f;
+					} else if (environmentalMods >= -0.8f) {
+						val = 0.3f;
+					} else if (environmentalMods >= -1.2f) {
+						val = 0.2f;
+					} else if (environmentalMods >= -1.6f) {
+						val = 0.1f;
+					}
+					return val;
 				}
 			}
-			float val = 0.0f;
-			if (environmentalMods >= 2.0f) {
-				val = 1.0f;
-			} else if (environmentalMods >= 1.6f) {
-				val = 0.9f;
-			} else if (environmentalMods >= 1.2f) {
-				val = 0.8f;
-			} else if (environmentalMods >= 0.8f) {
-				val = 0.7f;
-			} else if (environmentalMods >= 0.4f) {
-				val = 0.6f;
-			} else if (environmentalMods >= 0.0f) {
-				val = 0.5f;
-			} else if (environmentalMods >= -0.4f) {
-				val = 0.4f;
-			} else if (environmentalMods >= -0.8f) {
-				val = 0.3f;
-			} else if (environmentalMods >= -1.2f) {
-				val = 0.2f;
-			} else if (environmentalMods >= -1.6f) {
-				val = 0.1f;
-			}
-			System.out.println("Returned = "+val+", Actually = "+environmentalMods);
-			return val;
 		});
 	}
 }
