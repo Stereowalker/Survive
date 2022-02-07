1afb4297245408a8037a31fb3b9753ef0e544583
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2022-02-07 07:50:34 +0100

    Fix experimental hyp(o/er)thermia not dealing damage
    - It should now reduce health up to 1/4 of your max health

--- src/main/java/com/stereowalker/survive/world/effect/UnwellMobEffect.java
+++ src/main/java/com/stereowalker/survive/world/effect/UnwellMobEffect.java
@@ -3,14 +3,53 @@ package com.stereowalker.survive.world.effect;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.stereowalker.survive.needs.SDamageSource;
+
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 public class UnwellMobEffect extends MobEffect {
 
 	public UnwellMobEffect(MobEffectCategory effectType, int liquidColorIn) {
 		super(effectType, liquidColorIn);
+	}
+
+	@Override
+	public void applyEffectTick(LivingEntity entityLivingBaseIn, int amplifier) {
+		if (this == SEffects.HYPOTHERMIA && entityLivingBaseIn instanceof Player) {
+			if (entityLivingBaseIn.getHealth() > entityLivingBaseIn.getMaxHealth()/4.0F) {
+				entityLivingBaseIn.hurt(SDamageSource.HYPOTHERMIA, 1.0F);
+	         }
+		} else if (this == SEffects.HYPERTHERMIA && entityLivingBaseIn instanceof Player) {
+			if (entityLivingBaseIn.getHealth() > entityLivingBaseIn.getMaxHealth()/4.0F) {
+				entityLivingBaseIn.hurt(SDamageSource.HYPERTHERMIA, 1.0F);
+	         }
+		}
+		super.applyEffectTick(entityLivingBaseIn, amplifier);
+	}
+
+	@Override
+	public boolean isDurationEffectTick(int duration, int amplifier) {
+		if (this == SEffects.HYPOTHERMIA) {
+			int k = 120 >> amplifier;
+			if (k > 0) {
+				return duration % k == 0;
+			} else {
+				return true;
+			}
+		} else if (this == SEffects.HYPERTHERMIA) {
+			int k = 120 >> amplifier;
+			if (k > 0) {
+				return duration % k == 0;
+			} else {
+				return true;
+			}
+		} else {
+			return false;
+		}
 	}
 
 	@Override
