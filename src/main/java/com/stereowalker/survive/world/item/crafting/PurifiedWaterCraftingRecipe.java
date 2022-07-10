91bfce6572b3fc6fd1f382686814d229dca439b5
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2022-07-10 03:24:12 +0100

    Fix inability for clients to connect to servers

--- src/main/java/com/stereowalker/survive/world/item/crafting/PurifiedWaterCraftingRecipe.java
+++ src/main/java/com/stereowalker/survive/world/item/crafting/PurifiedWaterCraftingRecipe.java
@@ -1,25 +1,17 @@
 package com.stereowalker.survive.world.item.crafting;
 
-import com.stereowalker.survive.world.item.alchemy.SPotions;
 import com.stereowalker.unionlib.world.item.crafting.NoRemainderShaplessRecipe;
 
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 
 public class PurifiedWaterCraftingRecipe extends NoRemainderShaplessRecipe {
 
 	public PurifiedWaterCraftingRecipe(ResourceLocation pId, String pGroup, ItemStack pResult, NonNullList<Ingredient> pIngredients) {
-		super(pId, pGroup, pResult, add(pIngredients));
-	}
-	
-	private static NonNullList<Ingredient> add(NonNullList<Ingredient> ing){
-		ing.add(Ingredient.of(PotionUtils.setPotion(new ItemStack(Items.POTION), SPotions.PURIFIED_WATER)));
-		return ing;
+		super(pId, pGroup, pResult, pIngredients);
 	}
 
 	@Override
--- src/main/java/com/stereowalker/survive/world/item/crafting/PurifiedWaterCraftingSerializer.java
+++ src/main/java/com/stereowalker/survive/world/item/crafting/PurifiedWaterCraftingSerializer.java
@@ -3,30 +3,32 @@ package com.stereowalker.survive.world.item.crafting;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.stereowalker.survive.world.item.alchemy.SPotions;
 
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.ShapedRecipe;
 
 public class PurifiedWaterCraftingSerializer extends net.minecraftforge.registries.ForgeRegistryEntry<RecipeSerializer<?>> implements RecipeSerializer<PurifiedWaterCraftingRecipe> {
-    private static final ResourceLocation NAME = new ResourceLocation("minecraft", "crafting_shapeless");
     static int MAX_WIDTH = 3;
     static int MAX_HEIGHT = 3;
     public PurifiedWaterCraftingRecipe fromJson(ResourceLocation pRecipeId, JsonObject pJson) {
        String s = GsonHelper.getAsString(pJson, "group", "");
-       NonNullList<Ingredient> nonnulllist = itemsFromJson(GsonHelper.getAsJsonArray(pJson, "ingredients"));
+       NonNullList<Ingredient> nonnulllist = add(itemsFromJson(GsonHelper.getAsJsonArray(pJson, "ingredients")));
        if (nonnulllist.isEmpty()) {
           throw new JsonParseException("No ingredients for shapeless recipe");
        } else if (nonnulllist.size() > MAX_WIDTH * MAX_HEIGHT) {
           throw new JsonParseException("Too many ingredients for shapeless recipe. The maximum is " + (MAX_WIDTH * MAX_HEIGHT));
        } else {
           ItemStack itemstack = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(pJson, "result"));
-          return new PurifiedWaterCraftingRecipe(pRecipeId, s, itemstack, nonnulllist);
+          return new PurifiedWaterCraftingRecipe(pRecipeId, s, itemstack, add(nonnulllist));
        }
     }
 
@@ -53,17 +55,32 @@ public class PurifiedWaterCraftingSerializer extends net.minecraftforge.registri
        }
 
        ItemStack itemstack = pBuffer.readItem();
-       return new PurifiedWaterCraftingRecipe(pRecipeId, s, itemstack, nonnulllist);
+       return new PurifiedWaterCraftingRecipe(pRecipeId, s, itemstack, add(nonnulllist));
     }
 
     public void toNetwork(FriendlyByteBuf pBuffer, PurifiedWaterCraftingRecipe pRecipe) {
        pBuffer.writeUtf(pRecipe.getGroup());
-       pBuffer.writeVarInt(pRecipe.getIngredients().size());
+       NonNullList<Ingredient> nonnulllist = add(pRecipe.getIngredients());
+       pBuffer.writeVarInt(nonnulllist.size());
 
-       for(Ingredient ingredient : pRecipe.getIngredients()) {
+       for(Ingredient ingredient : nonnulllist) {
           ingredient.toNetwork(pBuffer);
        }
 
        pBuffer.writeItem(pRecipe.getResultItem());
     }
+	
+	protected static NonNullList<Ingredient> add(NonNullList<Ingredient> ing){
+		ItemStack potion = PotionUtils.setPotion(new ItemStack(Items.POTION), SPotions.PURIFIED_WATER);
+		if (ing.get(ing.size()-1).test(potion)) {
+			return ing;
+		} else {
+			NonNullList<Ingredient> nonnulllist = NonNullList.withSize(ing.size()+1, Ingredient.EMPTY);
+			for(int j = 0; j < ing.size(); ++j) {
+				nonnulllist.set(j, ing.get(j));
+			}
+			nonnulllist.set(ing.size(), Ingredient.of(potion));
+			return nonnulllist;
+		}
+	}
  }
