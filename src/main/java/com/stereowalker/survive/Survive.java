eda618b6de110fcd84b9fd73b48053fcb9889ea7
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2022-10-17 12:44:00 +0100

    Updated to 3.4.8

--- src/main/java/com/stereowalker/survive/Survive.java
+++ src/main/java/com/stereowalker/survive/Survive.java
@@ -80,7 +80,7 @@ public class Survive extends UnionMod {
 	
 	public Survive() 
 	{
-		super("survive", new ResourceLocation(MOD_ID, "textures/icon.png"), LoadType.BOTH);Entity.
+		super("survive", new ResourceLocation(MOD_ID, "textures/icon.png"), LoadType.BOTH);
 		instance = this;
 		ConfigBuilder.registerConfig(Config.class);
 		ConfigBuilder.registerConfig(ServerConfig.class);
