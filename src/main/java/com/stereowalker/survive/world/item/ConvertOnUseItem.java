2bc7ab426ace06dabe0343a68a46316af131c20f
Stereowalker <stereowalker.mc@gmail.com>
2024-07-18 18:35:58 +0100

    Allowed Bath Sponges to stack to 32 and added a new "Used Bath Sponge" when one is used

---
+++ src/main/java/com/stereowalker/survive/world/item/ConvertOnUseItem.java
@@ -0,0 +1,28 @@
+package com.stereowalker.survive.world.item;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class ConvertOnUseItem extends Item {
+	Item convertsTo;
+
+	public ConvertOnUseItem(Item convertsTo, Properties pProperties) {
+		super(pProperties);
+		this.convertsTo = convertsTo;
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+		ItemStack stack = pPlayer.getItemInHand(pUsedHand).copy();
+		stack.shrink(1);
+		ItemStack convertStack = new ItemStack(convertsTo);
+		pPlayer.setItemInHand(pUsedHand, convertStack);
+		pPlayer.addItem(stack);
+		return InteractionResultHolder.consume(convertStack);
+	}
+
+}
