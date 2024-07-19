719797ee2bb97bcf23c573d05f47ed632d5bdbb7
Stereowalker <stereowalker.mc@gmail.com>
2024-07-19 22:39:07 +0100

    Moved more events to inserts

--- src/main/java/com/stereowalker/survive/events/SleepEvents.java
+++ src/main/java/com/stereowalker/survive/events/SleepEvents.java
@@ -2,6 +2,7 @@ package com.stereowalker.survive.events;
 
 import com.stereowalker.survive.Survive;
 import com.stereowalker.survive.core.SurviveEntityStats;
+import com.stereowalker.survive.needs.SleepData;
 import com.stereowalker.survive.world.entity.ai.attributes.SAttributes;
 import com.stereowalker.unionlib.api.insert.InsertSetter;
 
@@ -9,6 +10,8 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.player.Player.BedSleepingProblem;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 
 public class SleepEvents {
 	public static void allowSleep(Player player, BlockPos pos, BedSleepingProblem vanillaProblem, InsertSetter<BedSleepingProblem> problem) {
@@ -30,6 +33,14 @@ public class SleepEvents {
 			else if (SurviveEntityStats.getEnergyStats(splayer).getEnergyLevel() < splayer.getAttributeValue(SAttributes.MAX_STAMINA.holder())/2) {
 				mayContinueSleeping.set(true);
 			}
+		}
+	}
+	
+	public static void replenishEnergy(LevelAccessor level) {
+		for (Player player : level.players()) {
+			SleepData stats = SurviveEntityStats.getSleepStats(player);
+			stats.setAwakeTimer(0);
+			stats.save(player);
 		}
 	}
 	
