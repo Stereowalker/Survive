faa650ca4a37ec166d6b4dabfda78eefb6313c83
Stereowalker <stereowalker.mc@gmail.com>
2024-08-08 01:04:26 +0100

    Improved the accuracy at which food lifespan is extended when cooled

--- src/main/java/com/stereowalker/survive/FoodUtils.java
+++ src/main/java/com/stereowalker/survive/FoodUtils.java
@@ -2,6 +2,8 @@ package com.stereowalker.survive;
 
 import java.util.List;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.stereowalker.survive.json.FoodJsonHolder;
 import com.stereowalker.survive.world.DataMaps;
 import com.stereowalker.survive.world.item.component.SDataComponents;
@@ -9,30 +11,56 @@ import com.stereowalker.unionlib.util.RegistryHelper;
 
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 public class FoodUtils {
+
+	public record FoodStatus(long creationTime, double lifespan) {
+		public static final Codec<FoodStatus> CODEC = RecordCodecBuilder.create(
+				builder -> builder.group(
+						Codec.LONG.fieldOf("creation_time").forGetter(FoodStatus::creationTime),
+						Codec.DOUBLE.fieldOf("lifespan").forGetter(FoodStatus::lifespan)
+						)
+				.apply(builder, FoodStatus::new)
+				);
+		public static final StreamCodec<RegistryFriendlyByteBuf, FoodStatus> STREAM_CODEC = StreamCodec.composite(
+				ByteBufCodecs.VAR_LONG,
+				FoodStatus::creationTime,
+				ByteBufCodecs.DOUBLE,
+				FoodStatus::lifespan,
+				FoodStatus::new
+				);
+		
+		public long expireTime() {return creationTime + (long)Math.floor(lifespan);}
+		
+		public FoodStatus extendTime(float extension) { return new FoodStatus(creationTime, lifespan + extension);}
+	}
+
+
 	public enum State {Fresh, Good, Okay, Spoiling, Spoiled}
 
 	public static void giveLifespanToFood(NonNullList<ItemStack> items, long gametime) {
 		if (Survive.FOOD_CONFIG.enabled) {
 			items.forEach((stack) -> {
-				if (stack.has(DataComponents.FOOD) && !stack.has(SDataComponents.EXPIRE_TIME) && DataMaps.Server.consummableItem.containsKey(RegistryHelper.items().getKey(stack.getItem()))) {
+				if (stack.has(DataComponents.FOOD) && !stack.has(SDataComponents.FOOD_STATUS) && DataMaps.Server.consummableItem.containsKey(RegistryHelper.items().getKey(stack.getItem()))) {
 					long lifespan = DataMaps.Server.consummableItem.get(RegistryHelper.items().getKey(stack.getItem())).lifespan();
 					if (lifespan > 0) {
 						long shaveAMinuteOff = gametime - (gametime % (20 * 60));
-						stack.set(SDataComponents.EXPIRE_TIME, shaveAMinuteOff + DataMaps.Server.consummableItem.get(RegistryHelper.items().getKey(stack.getItem())).lifespan());
+						stack.set(SDataComponents.FOOD_STATUS, new FoodStatus(shaveAMinuteOff, lifespan));
 					}
 				}
 			});
 		} else {
 			items.forEach((stack) -> {
-				if (stack.has(SDataComponents.EXPIRE_TIME)) {
-					stack.remove(SDataComponents.EXPIRE_TIME);
+				if (stack.has(SDataComponents.FOOD_STATUS)) {
+					stack.remove(SDataComponents.FOOD_STATUS);
 				}
 			});
 		}
@@ -40,16 +68,16 @@ public class FoodUtils {
 
 	public static void giveLifespanToFood(ItemStack stack, long gametime) {
 		if (Survive.FOOD_CONFIG.enabled) {
-			if (stack.has(DataComponents.FOOD) && !stack.has(SDataComponents.EXPIRE_TIME) && DataMaps.Server.consummableItem.containsKey(RegistryHelper.items().getKey(stack.getItem()))) {
+			if (stack.has(DataComponents.FOOD) && !stack.has(SDataComponents.FOOD_STATUS) && DataMaps.Server.consummableItem.containsKey(RegistryHelper.items().getKey(stack.getItem()))) {
 				long lifespan = DataMaps.Server.consummableItem.get(RegistryHelper.items().getKey(stack.getItem())).lifespan();
 				if (lifespan > 0) {
 					long shaveAMinuteOff = gametime - (gametime % (20 * 60));
-					stack.set(SDataComponents.EXPIRE_TIME, shaveAMinuteOff + DataMaps.Server.consummableItem.get(RegistryHelper.items().getKey(stack.getItem())).lifespan());
+					stack.set(SDataComponents.FOOD_STATUS, new FoodStatus(shaveAMinuteOff, lifespan));
 				}
 			}
 		} else {
-			if (stack.has(SDataComponents.EXPIRE_TIME)) {
-				stack.remove(SDataComponents.EXPIRE_TIME);
+			if (stack.has(SDataComponents.FOOD_STATUS)) {
+				stack.remove(SDataComponents.FOOD_STATUS);
 			}
 		}
 	}
@@ -71,9 +99,9 @@ public class FoodUtils {
 	}
 
 	public static State foodStatus(ItemStack stack, Level level) {
-		if (stack.has(SDataComponents.EXPIRE_TIME) && (level.isClientSide ?  DataMaps.Client.consummableItem : DataMaps.Server.consummableItem).containsKey(RegistryHelper.items().getKey(stack.getItem())) && Survive.FOOD_CONFIG.enabled) {
+		if (stack.has(SDataComponents.FOOD_STATUS) && (level.isClientSide ?  DataMaps.Client.consummableItem : DataMaps.Server.consummableItem).containsKey(RegistryHelper.items().getKey(stack.getItem())) && Survive.FOOD_CONFIG.enabled) {
 			FoodJsonHolder food = (level.isClientSide ?  DataMaps.Client.consummableItem : DataMaps.Server.consummableItem).get(RegistryHelper.items().getKey(stack.getItem()));
-			long timeTill = stack.get(SDataComponents.EXPIRE_TIME) - level.getGameTime();
+			long timeTill = stack.get(SDataComponents.FOOD_STATUS).expireTime() - level.getGameTime();
 			long timeSince = food.lifespan() - timeTill;
 			if (timeTill < 0) {
 				return State.Spoiled;
