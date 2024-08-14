d2def52805b732a94a0c6824972eede92aea7f3e
Stereowalker <stereowalker.mc@gmail.com>
2024-08-14 23:46:43 +0100

    Used unionlib to handle block and item colors

--- src/main/java/com/stereowalker/survive/core/registries/SurviveRegistryEvents.java
+++ src/main/java/com/stereowalker/survive/core/registries/SurviveRegistryEvents.java
@@ -8,12 +8,8 @@ import com.stereowalker.survive.Survive;
 import com.stereowalker.survive.client.particle.HygieneParticle;
 import com.stereowalker.survive.core.particles.SParticleTypes;
 import com.stereowalker.survive.world.entity.ai.attributes.SAttributes;
-import com.stereowalker.survive.world.item.SItems;
-import com.stereowalker.survive.world.item.TemperatureRegulatorPlateItem;
 import com.stereowalker.survive.world.item.alchemy.SPotions;
 import com.stereowalker.survive.world.item.crafting.conditions.ModuleEnabledCondition;
-import com.stereowalker.survive.world.level.block.PlatedTemperatureRegulatorBlock;
-import com.stereowalker.survive.world.level.block.SBlocks;
 import com.stereowalker.survive.world.level.material.PurifiedWaterFluid;
 import com.stereowalker.survive.world.level.material.SFluids;
 import com.stereowalker.survive.world.level.storage.loot.predicates.SLootItemConditions;
@@ -23,19 +19,14 @@ import com.stereowalker.unionlib.util.RegistryHelper;
 import com.stereowalker.unionlib.util.VersionHelper;
 
 import net.minecraft.core.Holder;
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.util.FastColor;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.neoforged.neoforge.registries.NewRegistryEvent;
@@ -47,30 +38,6 @@ public class SurviveRegistryEvents
 	private static final int MAX_VARINT = Integer.MAX_VALUE - 1;
 	//Game Object Registries
 	@SubscribeEvent
-	public static void registerItemColors(RegisterColorHandlersEvent.Block event) {
-		event.register((state, displayReader, blockPos, tintIndex) -> {
-			return Survive.PURIFIED_WATER_COLOR;
-		}, SBlocks.PURIFIED_WATER, SBlocks.PURIFIED_WATER_CAULDRON);
-		event.register((state, displayReader, blockPos, tintIndex) -> {
-			return 0x483c35;
-		}, SBlocks.POTASH_CAULDRON);
-		event.register((state, displayReader, blockPos, tintIndex) -> {
-			return PlatedTemperatureRegulatorBlock.getColor(state);
-		}, SBlocks.PLATED_TEMPERATURE_REGULATOR);
-	}
-	
-	@SubscribeEvent
-	public static void registerItemColors(RegisterColorHandlersEvent.Item event) {
-		event.register((stack, tintIndex) -> {
-			PotionContents contents = stack.getOrDefault(DataComponents.POTION_CONTENTS, PotionContents.EMPTY);
-			return tintIndex > 0 ? -1 : contents.is(SPotions.PURIFIED_WATER.holder()) ? Survive.PURIFIED_WATER_COLOR : FastColor.ARGB32.opaque(contents.getColor());
-	      }, Items.POTION, Items.SPLASH_POTION, Items.LINGERING_POTION);
-		event.register((stack, tintIndex) -> {
-			return TemperatureRegulatorPlateItem.getColor(stack);
-		}, SItems.LARGE_HEATING_PLATE, SItems.LARGE_COOLING_PLATE, SItems.MEDIUM_HEATING_PLATE, SItems.MEDIUM_COOLING_PLATE, SItems.SMALL_HEATING_PLATE, SItems.SMALL_COOLING_PLATE);
-	}
-
-	@SubscribeEvent
 	public static void registerParticleFactories(RegisterParticleProvidersEvent event) {
 		event.registerSpriteSet(SParticleTypes.STINK, HygieneParticle.StinkFactory::new);
 		event.registerSpriteSet(SParticleTypes.CLEAN, HygieneParticle.CleanFactory::new);
@@ -79,8 +46,8 @@ public class SurviveRegistryEvents
 	@SubscribeEvent
 	public static void registerParticlesz(final RegisterEvent event) {
 		event.register(RegistryHelper.particleTypeKey(), (helper) -> SParticleTypes.registerAll(helper));
-		event.register(SurviveRegistries.CONDITION, (helper) -> TemperatureChangeConditions.registerAll(helper));
-		event.register(SurviveRegistries.SEASON, (helper) -> Seasons.registerAll(helper));
+		event.register(SurviveRegistries.Keys.CONDITION, (helper) -> TemperatureChangeConditions.registerAll(helper));
+		event.register(SurviveRegistries.Keys.SEASON, (helper) -> Seasons.registerAll(helper));
 		event.register(NeoForgeRegistries.Keys.FLUID_TYPES, (helper) -> helper.register(VersionHelper.toLoc("survive:purified_water"), PurifiedWaterFluid.TYPE));
 //		if (event.getRegistryKey().equals(ForgeRegistries.Keys.RECIPE_SERIALIZERS))
 //        {			
