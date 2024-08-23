07a6358f04ae99883e2858dcfa29ba39f9890cc8
Stereowalker <stereowalker.mc@gmail.com>
2024-08-23 01:04:44 +0100

    Update JEI version

--- src/main/java/com/stereowalker/survive/compat/jei/JEICompat.java
+++ src/main/java/com/stereowalker/survive/compat/jei/JEICompat.java
@@ -16,7 +16,6 @@ import mezz.jei.api.helpers.IJeiHelpers;
 import mezz.jei.api.helpers.IStackHelper;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.ISubtypeRegistration;
-import mezz.jei.library.plugins.vanilla.brewing.PotionSubtypeInterpreter;
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
---
+++ src/main/java/com/stereowalker/survive/compat/jei/PotionSubtypeInterpreter.java
@@ -0,0 +1,27 @@
+package com.stereowalker.survive.compat.jei;
+
+import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
+import mezz.jei.api.ingredients.subtypes.UidContext;
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.PotionContents;
+
+public class PotionSubtypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack> {
+	public static final PotionSubtypeInterpreter INSTANCE = new PotionSubtypeInterpreter();
+
+	private PotionSubtypeInterpreter() {
+
+	}
+
+	@Override
+	public String apply(ItemStack itemStack, UidContext context) {
+		if (itemStack.getComponentsPatch().isEmpty()) {
+			return IIngredientSubtypeInterpreter.NONE;
+		}
+		PotionContents contents = itemStack.getOrDefault(DataComponents.POTION_CONTENTS, PotionContents.EMPTY);
+		String itemDescriptionId = itemStack.getItem().getDescriptionId();
+		String potionEffectId = contents.potion().map(Holder::getRegisteredName).orElse("none");
+		return itemDescriptionId + ".effect_id." + potionEffectId;
+	}
+}
