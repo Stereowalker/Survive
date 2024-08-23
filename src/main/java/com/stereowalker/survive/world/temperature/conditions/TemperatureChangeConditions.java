4beec1dbd45adc1ab78ab5c965e94e85d0895225
Stereowalker <stereowalker.mc@gmail.com>
2024-08-23 18:43:34 +0100

    Use inserts for use item

--- src/main/java/com/stereowalker/survive/world/temperature/conditions/TemperatureChangeConditions.java
+++ src/main/java/com/stereowalker/survive/world/temperature/conditions/TemperatureChangeConditions.java
@@ -6,6 +6,7 @@ import java.util.Map.Entry;
 
 import com.stereowalker.survive.Survive;
 import com.stereowalker.unionlib.api.registries.RegistryCollector;
+import com.stereowalker.unionlib.util.VersionHelper;
 
 import net.minecraft.resources.ResourceLocation;
 
@@ -19,15 +20,15 @@ public class TemperatureChangeConditions {
 	public static final TemperatureChangeCondition<?> NOT_BIOME = register("not_biome", new BiomeNotCondition());
 	
 	public static TemperatureChangeCondition<?> register(String name, TemperatureChangeCondition<?> condition) {
-		CONDITION_LIST.put(Survive.getInstance().location(name), condition);
+		CONDITION_LIST.put(VersionHelper.toLoc(Survive.MOD_ID, name), condition);
 		return condition;
 	}
 	
 	public static void registerAll(RegistryCollector.Custom<TemperatureChangeCondition<?>> registry) {
 		for(Entry<ResourceLocation, TemperatureChangeCondition<?>> condition : CONDITION_LIST.entrySet()) {
 			registry.register(condition.getKey(), condition.getValue());
-			Survive.getInstance().debug("Condition: \""+condition.getKey().toString()+"\" registered");
+//			Survive.getInstance().debug("Condition: \""+condition.getKey().toString()+"\" registered");
 		}
-		Survive.getInstance().debug("All Conditions Registered");
+//		Survive.getInstance().debug("All Conditions Registered");
 	}
 }
