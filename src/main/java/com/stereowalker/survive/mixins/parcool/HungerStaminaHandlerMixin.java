1ed08b7f98c3693107763fbab9b0096bda35a999
Stereowalker <stereowalker.mc@gmail.com>
2024-11-17 04:03:35 +0100

    Added integration with parcool

---
+++ src/main/java/com/stereowalker/survive/mixins/parcool/HungerStaminaHandlerMixin.java
@@ -0,0 +1,71 @@
+package com.stereowalker.survive.mixins.parcool;
+//
+import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.Overwrite;
+//import org.spongepowered.asm.mixin.Shadow;
+//
+//import com.alrex.parcool.common.attachment.stamina.ReadonlyStamina;
+//import com.alrex.parcool.common.network.payload.StaminaProcessOnServerPayload;
+//import com.alrex.parcool.common.stamina.IParCoolStaminaHandler;
+//import com.alrex.parcool.common.stamina.StaminaType;
+//import com.alrex.parcool.common.stamina.handlers.HungerStaminaHandler;
+//import com.stereowalker.survive.Survive;
+//import com.stereowalker.survive.api.needs.PlayerNeeds;
+//import com.stereowalker.survive.needs.StaminaData;
+//
+//import net.minecraft.client.player.LocalPlayer;
+//import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.world.entity.player.Player;
+//import net.minecraftforge.api.distmarker.Dist;
+//import net.minecraftforge.api.distmarker.OnlyIn;
+//import net.neoforged.neoforge.network.PacketDistributor;
+//
+@Mixin(Player.class)
+public class HungerStaminaHandlerMixin /* implements IParCoolStaminaHandler */ {
+//	@Shadow private int consumed = 0;
+//
+//	@Overwrite
+//	@OnlyIn(value = Dist.CLIENT)
+//	public ReadonlyStamina initializeStamina(LocalPlayer player, ReadonlyStamina current) {
+//		if (Survive.STAMINA_CONFIG.parcool)
+//			return new ReadonlyStamina(false, PlayerNeeds.api().getStamina(player).getBurstStamina(), 20);
+//		else
+//			return new ReadonlyStamina(false, player.getFoodData().getFoodLevel(), 20);
+//	}
+//
+//	@Overwrite
+//	@OnlyIn(value = Dist.CLIENT)
+//	public ReadonlyStamina consume(LocalPlayer player, ReadonlyStamina current, int value) {
+//		this.consumed += value;
+//		return current;
+//	}
+//
+//	@Overwrite
+//	@OnlyIn(value = Dist.CLIENT)
+//	public ReadonlyStamina recover(LocalPlayer player, ReadonlyStamina current, int value) {
+//		return current;
+//	}
+//
+//	@Overwrite
+//	@OnlyIn(value = Dist.CLIENT)
+//	public ReadonlyStamina onTick(LocalPlayer player, ReadonlyStamina current) {
+//		if (this.consumed > 0) {
+//			PacketDistributor.sendToServer(
+//					(CustomPacketPayload) new StaminaProcessOnServerPayload(StaminaType.HUNGER, this.consumed),
+//					(CustomPacketPayload[]) new CustomPacketPayload[0]);
+//			this.consumed = 0;
+//		}
+//		if (Survive.STAMINA_CONFIG.parcool)
+//			return new ReadonlyStamina(((StaminaData)PlayerNeeds.api().getStamina(player)).isShortOfBreath(), PlayerNeeds.api().getStamina(player).getBurstStamina(), 20);
+//		else
+//			return new ReadonlyStamina(player.getFoodData().getFoodLevel() < 6, player.getFoodData().getFoodLevel(), 20);
+//	}
+//
+//	@Overwrite
+//	public void processOnServer(Player player, int value) {
+//		if (Survive.STAMINA_CONFIG.parcool)
+//			((StaminaData)PlayerNeeds.api().getStamina(player)).addExhaustion((float) value / 1000.0f, true);
+//		else
+//			player.causeFoodExhaustion((float) value / 1000.0f);
+//	}
+}
