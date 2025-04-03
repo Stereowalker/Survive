58c1ba84a22ad9e38d1a03b0326decaa3e0fb217
Stereowalker <stereowalker.mc@gmail.com>
2025-04-03 05:48:59 +0100

    Used UnionLib to handle fluids

--- src/main/java/com/stereowalker/survive/core/registries/SurviveRegistryEvents.java
+++ src/main/java/com/stereowalker/survive/core/registries/SurviveRegistryEvents.java
@@ -1,27 +1,11 @@
 package com.stereowalker.survive.core.registries;
 
-import java.util.List;
-
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import com.stereowalker.survive.Survive;
-import com.stereowalker.survive.client.particle.HygieneParticle;
-import com.stereowalker.survive.core.particles.SParticleTypes;
-import com.stereowalker.survive.world.item.alchemy.SPotions;
 import com.stereowalker.survive.world.item.crafting.conditions.ModuleEnabledCondition;
-import com.stereowalker.survive.world.level.material.PurifiedWaterFluid;
-import com.stereowalker.survive.world.level.material.SFluids;
 import com.stereowalker.survive.world.level.storage.loot.predicates.SLootItemConditions;
 import com.stereowalker.unionlib.util.VersionHelper;
 
-import net.minecraft.core.Holder;
-import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.Potions;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.Fluids;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.neoforged.neoforge.registries.RegisterEvent;
 
@@ -31,7 +15,6 @@ public class SurviveRegistryEvents
 	
 	@SubscribeEvent
 	public static void registerParticlesz(final RegisterEvent event) {
-		event.register(NeoForgeRegistries.Keys.FLUID_TYPES, (helper) -> helper.register(VersionHelper.toLoc("survive:purified_water"), PurifiedWaterFluid.TYPE));
 		event.register(NeoForgeRegistries.Keys.CONDITION_CODECS, (reg) -> {
 			reg.register(VersionHelper.toLoc("survive", "module_enabled"), ModuleEnabledCondition.CODEC);
 		});
