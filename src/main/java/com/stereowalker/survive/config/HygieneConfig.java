04bea7ef59c23d6e197a35ab694adc5d17fa7d2f
Stereowalker <stereowalker.mc@gmail.com>
2025-04-06 17:25:40 +0100

    Doubled the amount of grime stacks required to fall ill

--- src/main/java/com/stereowalker/survive/config/HygieneConfig.java
+++ src/main/java/com/stereowalker/survive/config/HygieneConfig.java
@@ -15,5 +15,12 @@ public class HygieneConfig implements ConfigObject {
 	@UnionConfig.Comment(comment = {"How many ticks should pass before the players grime counter increases by one","Setting this to -1 prevents the player from getting dirty this way"})
 	@UnionConfig.Range(min = -1, max = 5000, useSlider = true)
 	public int dirtyTickRate = 600;
+	
+	@UnionConfig.Entry(name = "Illness Level", side = ConfigSide.Shared)
+	@UnionConfig.Comment(comment = {"How many dirty stacks would cause the player to turn sick",
+			"Setting this to -1 prevents the player from getting sick this way if the wellbeing module is enabled",
+			"Keep in mind that the player becomes dirty at 30 stacks"})
+	@UnionConfig.Range(min = -1, max = 5000, useSlider = true)
+	public int illnessLevel = 200;
 
 }
