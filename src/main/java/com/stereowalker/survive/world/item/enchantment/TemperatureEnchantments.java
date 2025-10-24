d6d233748743a19b5137d4ec63058a1d7e51f3be
Stereowalker <stereowalker.mc@gmail.com>
2025-10-24 06:39:03 +0100

    Updated UnionLib to 12.1.0

--- src/main/java/com/stereowalker/survive/world/item/enchantment/TemperatureEnchantments.java
+++ src/main/java/com/stereowalker/survive/world/item/enchantment/TemperatureEnchantments.java
@@ -1,5 +1,6 @@
 package com.stereowalker.survive.world.item.enchantment;
 
+import com.stereowalker.survive.Survive;
 import com.stereowalker.unionlib.core.registries.RegistryHolder;
 import com.stereowalker.unionlib.core.registries.RegistryObject;
 
@@ -7,7 +8,7 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantment.Rarity;
 
-@RegistryHolder(registry = Enchantment.class)
+@RegistryHolder(namespace = Survive.MOD_ID)
 public class TemperatureEnchantments {
 	@RegistryObject("warming")
 	public static final Enchantment WARMING = new TempControlEnchantment(Rarity.UNCOMMON/*Enchantment.definition(ItemTags.ARMOR_ENCHANTABLE, 10, 7, Enchantment.dynamicCost(6, 5), Enchantment.dynamicCost(26, 5), 1*/, new EquipmentSlot[] {EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET})/*)*/;
