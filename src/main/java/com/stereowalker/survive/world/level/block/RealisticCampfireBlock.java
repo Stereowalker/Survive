e3631ed975b2364f07b109276e29c17d1b6c6e27
Stereowalker <stereowalker.mc@gmail.com>
2025-10-26 02:44:35 +0100

    Made campfires behave a bit more realistically

---
+++ src/main/java/com/stereowalker/survive/world/level/block/RealisticCampfireBlock.java
@@ -0,0 +1,224 @@
+package com.stereowalker.survive.world.level.block;
+
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+
+import com.stereowalker.survive.world.level.block.entity.RealisticCampfireBlockEntity;
+import com.stereowalker.survive.world.level.block.entity.SBlockEntityType;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.crafting.CampfireCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CampfireBlock;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class RealisticCampfireBlock extends CampfireBlock {
+    public static final BooleanProperty LIT = BlockStateProperties.LIT;
+    public static final IntegerProperty HEAT = IntegerProperty.create("heat", 0, 4);
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    private static final VoxelShape VIRTUAL_FENCE_POST = Block.box(6.0, 0.0, 6.0, 10.0, 16.0, 10.0);
+    private static final int SMOKE_DISTANCE = 5;
+
+
+    public RealisticCampfireBlock(boolean p_51236_, int p_51237_, BlockBehaviour.Properties p_51238_) {
+        super(p_51236_, p_51237_, p_51238_);
+        this.registerDefaultState(
+            this.stateDefinition
+                .any().setValue(LIT, false).setValue(HEAT, 0)
+                .setValue(SIGNAL_FIRE, Boolean.valueOf(false))
+                .setValue(WATERLOGGED, Boolean.valueOf(false))
+                .setValue(FACING, Direction.NORTH)
+        );
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(
+        ItemStack pStack, BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHitResult
+    ) {
+        if (pLevel.getBlockEntity(pPos) instanceof RealisticCampfireBlockEntity campfireblockentity) {
+            ItemStack itemstack = pPlayer.getItemInHand(pHand);
+            Optional<RecipeHolder<CampfireCookingRecipe>> optional = campfireblockentity.getCookableRecipe(itemstack);
+            if (optional.isPresent()) {
+                if (!pLevel.isClientSide && campfireblockentity.placeFood(pPlayer, itemstack, optional.get().value().getCookingTime())) {
+                    pPlayer.awardStat(Stats.INTERACT_WITH_CAMPFIRE);
+                    return ItemInteractionResult.SUCCESS;
+                }
+
+                return ItemInteractionResult.CONSUME;
+            }
+            if (AbstractFurnaceBlockEntity.isFuel(pStack)) {
+            	if (!pLevel.isClientSide && campfireblockentity.placeFuel(pPlayer, itemstack)) {
+                    pPlayer.awardStat(Stats.INTERACT_WITH_CAMPFIRE);
+                    return ItemInteractionResult.SUCCESS;
+                }
+
+                return ItemInteractionResult.CONSUME;
+            }
+        }
+
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+    }
+
+    @Override
+    protected void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+        if (pState.getValue(HEAT) > 0 && pEntity instanceof LivingEntity) {
+            pEntity.hurt(pLevel.damageSources().campfire(), pState.getValue(HEAT) * .25f);
+        }
+
+        super.entityInside(pState, pLevel, pPos, pEntity);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        LevelAccessor levelaccessor = pContext.getLevel();
+        BlockPos blockpos = pContext.getClickedPos();
+        boolean flag = levelaccessor.getFluidState(blockpos).getType() == Fluids.WATER;
+        return this.defaultBlockState()
+            .setValue(WATERLOGGED, Boolean.valueOf(flag))
+            .setValue(SIGNAL_FIRE, Boolean.valueOf(this.isSmokeSource(levelaccessor.getBlockState(blockpos.below()))))
+            .setValue(LIT, false).setValue(HEAT, 0)
+            .setValue(FACING, pContext.getHorizontalDirection());
+    }
+
+    @Override
+    protected BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
+        if (pState.getValue(WATERLOGGED)) {
+            pLevel.scheduleTick(pCurrentPos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
+        }
+
+        return pFacing == Direction.DOWN
+            ? pState.setValue(SIGNAL_FIRE, Boolean.valueOf(this.isSmokeSource(pFacingState)))
+            : super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
+    }
+
+    private boolean isSmokeSource(BlockState pState) {
+        return pState.is(Blocks.HAY_BLOCK);
+    }
+
+    public static void dowse(@Nullable Entity pEntity, LevelAccessor pLevel, BlockPos pPos, BlockState pState) {
+        if (pLevel.isClientSide()) {
+            for (int i = 0; i < 20; i++) {
+                makeParticles((Level)pLevel, pPos, pState.getValue(SIGNAL_FIRE), true);
+            }
+        }
+
+        BlockEntity blockentity = pLevel.getBlockEntity(pPos);
+        if (blockentity instanceof RealisticCampfireBlockEntity) {
+            ((RealisticCampfireBlockEntity)blockentity).dowse();
+        }
+
+        pLevel.gameEvent(pEntity, GameEvent.BLOCK_CHANGE, pPos);
+    }
+
+    @Override
+    public boolean placeLiquid(LevelAccessor pLevel, BlockPos pPos, BlockState pState, FluidState pFluidState) {
+        if (!pState.getValue(BlockStateProperties.WATERLOGGED) && pFluidState.getType() == Fluids.WATER) {
+            boolean flag = pState.getValue(HEAT) > 0;
+            if (flag) {
+                if (!pLevel.isClientSide()) {
+                    pLevel.playSound(null, pPos, SoundEvents.GENERIC_EXTINGUISH_FIRE, SoundSource.BLOCKS, 1.0F, 1.0F);
+                }
+
+                dowse(null, pLevel, pPos, pState);
+            }
+
+            pLevel.setBlock(pPos, pState.setValue(WATERLOGGED, Boolean.valueOf(true)).setValue(LIT, false).setValue(HEAT, 0), 3);
+            pLevel.scheduleTick(pPos, pFluidState.getType(), pFluidState.getType().getTickDelay(pLevel));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onProjectileHit(Level pLevel, BlockState pState, BlockHitResult pHit, Projectile pProjectile) {
+        BlockPos blockpos = pHit.getBlockPos();
+        if (!pLevel.isClientSide && pProjectile.isOnFire() && pProjectile.mayInteract(pLevel, blockpos) && !pState.getValue(LIT) && !pState.getValue(WATERLOGGED)) {
+            pLevel.setBlock(blockpos, pState.setValue(BlockStateProperties.LIT, Boolean.valueOf(true)), 11);
+        }
+    }
+
+    public static void makeParticles(Level pLevel, BlockPos pPos, boolean pIsSignalFire, boolean pSpawnExtraSmoke) {
+        RandomSource randomsource = pLevel.getRandom();
+        SimpleParticleType simpleparticletype = pIsSignalFire ? ParticleTypes.CAMPFIRE_SIGNAL_SMOKE : ParticleTypes.CAMPFIRE_COSY_SMOKE;
+        pLevel.addAlwaysVisibleParticle(
+            simpleparticletype,
+            true,
+            (double)pPos.getX() + 0.5 + randomsource.nextDouble() / 3.0 * (double)(randomsource.nextBoolean() ? 1 : -1),
+            (double)pPos.getY() + randomsource.nextDouble() + randomsource.nextDouble(),
+            (double)pPos.getZ() + 0.5 + randomsource.nextDouble() / 3.0 * (double)(randomsource.nextBoolean() ? 1 : -1),
+            0.0,
+            0.07,
+            0.0
+        );
+        if (pSpawnExtraSmoke) {
+            pLevel.addParticle(
+                ParticleTypes.SMOKE,
+                (double)pPos.getX() + 0.5 + randomsource.nextDouble() / 4.0 * (double)(randomsource.nextBoolean() ? 1 : -1),
+                (double)pPos.getY() + 0.4,
+                (double)pPos.getZ() + 0.5 + randomsource.nextDouble() / 4.0 * (double)(randomsource.nextBoolean() ? 1 : -1),
+                0.0,
+                0.005,
+                0.0
+            );
+        }
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(LIT, SIGNAL_FIRE, WATERLOGGED, FACING, HEAT);
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+        return new RealisticCampfireBlockEntity(pPos, pState);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+        if (pLevel.isClientSide) {
+            return pState.getValue(HEAT) > 0 ? createTickerHelper(pBlockEntityType, SBlockEntityType.REALISIC_CAMPFIRE, RealisticCampfireBlockEntity::particleTick) : null;
+        } else {
+            return pState.getValue(HEAT) > 0
+                ? createTickerHelper(pBlockEntityType, SBlockEntityType.REALISIC_CAMPFIRE, RealisticCampfireBlockEntity::cookTick)
+                : createTickerHelper(pBlockEntityType, SBlockEntityType.REALISIC_CAMPFIRE, RealisticCampfireBlockEntity::cooldownTick);
+        }
+    }
+}
---
+++ src/main/java/com/stereowalker/survive/world/level/block/entity/RealisticCampfireBlockEntity.java
@@ -0,0 +1,263 @@
+package com.stereowalker.survive.world.level.block.entity;
+
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+
+import com.stereowalker.survive.world.level.block.RealisticCampfireBlock;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.Clearable;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.Containers;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.ItemContainerContents;
+import net.minecraft.world.item.crafting.CampfireCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SingleRecipeInput;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+
+public class RealisticCampfireBlockEntity extends BlockEntity implements Clearable {
+    private static final int BURN_COOL_SPEED = 2;
+    private static final int NUM_SLOTS = 4;
+    private final NonNullList<ItemStack> items = NonNullList.withSize(4, ItemStack.EMPTY);
+    private final int[] cookingProgress = new int[4];
+    private final int[] cookingTime = new int[4];
+    private int fuelTime = 0;
+    private final RecipeManager.CachedCheck<SingleRecipeInput, CampfireCookingRecipe> quickCheck = RecipeManager.createCheck(RecipeType.CAMPFIRE_COOKING);
+
+    public RealisticCampfireBlockEntity(BlockPos pPos, BlockState pBlockState) {
+        super(SBlockEntityType.REALISIC_CAMPFIRE, pPos, pBlockState);
+    }
+
+    public static void cookTick(Level pLevel, BlockPos pPos, BlockState pState, RealisticCampfireBlockEntity pBlockEntity) {
+        boolean flag = false;
+
+        for (int i = 0; i < pBlockEntity.items.size(); i++) {
+            ItemStack itemstack = pBlockEntity.items.get(i);
+            if (!itemstack.isEmpty()) {
+                flag = true;
+                pBlockEntity.cookingProgress[i]++;
+                if (pBlockEntity.cookingProgress[i] >= pBlockEntity.cookingTime[i]) {
+                    SingleRecipeInput singlerecipeinput = new SingleRecipeInput(itemstack);
+                    ItemStack itemstack1 = pBlockEntity.quickCheck
+                        .getRecipeFor(singlerecipeinput, pLevel)
+                        .map(p_341839_ -> p_341839_.value().assemble(singlerecipeinput, pLevel.registryAccess()))
+                        .orElse(itemstack);
+                    if (itemstack1.isItemEnabled(pLevel.enabledFeatures())) {
+                        Containers.dropItemStack(pLevel, (double)pPos.getX(), (double)pPos.getY(), (double)pPos.getZ(), itemstack1);
+                        pBlockEntity.items.set(i, ItemStack.EMPTY);
+                        pLevel.sendBlockUpdated(pPos, pState, pState, 3);
+                        pLevel.gameEvent(GameEvent.BLOCK_CHANGE, pPos, GameEvent.Context.of(pState));
+                    }
+                }
+            }
+        }
+
+        ///
+        flag = true;
+        pBlockEntity.fuelTime--;
+        if (pBlockEntity.fuelTime <= 0 || pLevel.isRainingAt(pPos.above()) || !pState.getValue(RealisticCampfireBlock.LIT)) {
+        	pLevel.setBlock(pPos, pState.setValue(RealisticCampfireBlock.LIT, false).setValue(RealisticCampfireBlock.HEAT, 0), 3);
+        }
+        else {
+        	if (pBlockEntity.fuelTime >= 750 * 3)
+        		pLevel.setBlock(pPos, pState.setValue(RealisticCampfireBlock.HEAT, 4), 3);
+        	else if (pBlockEntity.fuelTime >= 750 * 2)
+        		pLevel.setBlock(pPos, pState.setValue(RealisticCampfireBlock.HEAT, 3), 3);
+        	else if (pBlockEntity.fuelTime >= 750)
+        		pLevel.setBlock(pPos, pState.setValue(RealisticCampfireBlock.HEAT, 2), 3);
+        	else if (pBlockEntity.fuelTime >= 0)
+        		pLevel.setBlock(pPos, pState.setValue(RealisticCampfireBlock.HEAT, 1), 3);
+        }
+        
+        if (flag) {
+            setChanged(pLevel, pPos, pState);
+        }
+    }
+
+    public static void cooldownTick(Level pLevel, BlockPos pPos, BlockState pState, RealisticCampfireBlockEntity pBlockEntity) {
+        boolean flag = false;
+        ///
+        if (pBlockEntity.fuelTime <= 0) {
+        	pLevel.setBlock(pPos, pState.setValue(RealisticCampfireBlock.LIT, false).setValue(RealisticCampfireBlock.HEAT, 0), 3);
+        }
+        else if (pState.getValue(RealisticCampfireBlock.LIT) && pBlockEntity.fuelTime > 0 && !pLevel.isRainingAt(pPos)) {
+        	pLevel.setBlock(pPos, pState.setValue(RealisticCampfireBlock.HEAT, Mth.ceil(pBlockEntity.fuelTime / 1000f)), 3);
+        }
+        ///
+
+        for (int i = 0; i < pBlockEntity.items.size(); i++) {
+            if (pBlockEntity.cookingProgress[i] > 0) {
+                flag = true;
+                pBlockEntity.cookingProgress[i] = Mth.clamp(pBlockEntity.cookingProgress[i] - 2, 0, pBlockEntity.cookingTime[i]);
+            }
+        }
+
+        if (flag) {
+            setChanged(pLevel, pPos, pState);
+        }
+    }
+
+    public static void particleTick(Level pLevel, BlockPos pPos, BlockState pState, RealisticCampfireBlockEntity pBlockEntity) {
+        RandomSource randomsource = pLevel.random;
+        if (randomsource.nextFloat() < 0.11F) {
+            for (int i = 0; i < randomsource.nextInt(2) + 2; i++) {
+                RealisticCampfireBlock.makeParticles(pLevel, pPos, pState.getValue(RealisticCampfireBlock.SIGNAL_FIRE), false);
+            }
+        }
+
+        int l = pState.getValue(RealisticCampfireBlock.FACING).get2DDataValue();
+
+        for (int j = 0; j < pBlockEntity.items.size(); j++) {
+            if (!pBlockEntity.items.get(j).isEmpty() && randomsource.nextFloat() < 0.2F) {
+                Direction direction = Direction.from2DDataValue(Math.floorMod(j + l, 4));
+                float f = 0.3125F;
+                double d0 = (double)pPos.getX()
+                    + 0.5
+                    - (double)((float)direction.getStepX() * 0.3125F)
+                    + (double)((float)direction.getClockWise().getStepX() * 0.3125F);
+                double d1 = (double)pPos.getY() + 0.5;
+                double d2 = (double)pPos.getZ()
+                    + 0.5
+                    - (double)((float)direction.getStepZ() * 0.3125F)
+                    + (double)((float)direction.getClockWise().getStepZ() * 0.3125F);
+
+                for (int k = 0; k < 4; k++) {
+                    pLevel.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0.0, 5.0E-4, 0.0);
+                }
+            }
+        }
+    }
+
+    public NonNullList<ItemStack> getItems() {
+        return this.items;
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag pTag, HolderLookup.Provider pRegistries) {
+        super.loadAdditional(pTag, pRegistries);
+        this.items.clear();
+        ContainerHelper.loadAllItems(pTag, this.items, pRegistries);
+        if (pTag.contains("CookingTimes", 11)) {
+            int[] aint = pTag.getIntArray("CookingTimes");
+            System.arraycopy(aint, 0, this.cookingProgress, 0, Math.min(this.cookingTime.length, aint.length));
+        }
+
+        if (pTag.contains("CookingTotalTimes", 11)) {
+            int[] aint1 = pTag.getIntArray("CookingTotalTimes");
+            System.arraycopy(aint1, 0, this.cookingTime, 0, Math.min(this.cookingTime.length, aint1.length));
+        }
+        fuelTime = pTag.getInt("fuelTime");
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag pTag, HolderLookup.Provider pRegistries) {
+        super.saveAdditional(pTag, pRegistries);
+        ContainerHelper.saveAllItems(pTag, this.items, true, pRegistries);
+        pTag.putIntArray("CookingTimes", this.cookingProgress);
+        pTag.putIntArray("CookingTotalTimes", this.cookingTime);
+        pTag.putInt("fuelTime", fuelTime);
+    }
+
+    public ClientboundBlockEntityDataPacket getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider pRegistries) {
+        CompoundTag compoundtag = new CompoundTag();
+        ContainerHelper.saveAllItems(compoundtag, this.items, true, pRegistries);
+        return compoundtag;
+    }
+
+    public Optional<RecipeHolder<CampfireCookingRecipe>> getCookableRecipe(ItemStack pStack) {
+        return this.items.stream().noneMatch(ItemStack::isEmpty)
+            ? Optional.empty()
+            : this.quickCheck.getRecipeFor(new SingleRecipeInput(pStack), this.level);
+    }
+
+    public boolean placeFood(@Nullable LivingEntity pEntity, ItemStack pFood, int pCookTime) {
+        for (int i = 0; i < this.items.size(); i++) {
+            ItemStack itemstack = this.items.get(i);
+            if (itemstack.isEmpty()) {
+                this.cookingTime[i] = pCookTime;
+                this.cookingProgress[i] = 0;
+                this.items.set(i, pFood.consumeAndReturn(1, pEntity));
+                this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.getBlockPos(), GameEvent.Context.of(pEntity, this.getBlockState()));
+                this.markUpdated();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void markUpdated() {
+        this.setChanged();
+        this.getLevel().sendBlockUpdated(this.getBlockPos(), this.getBlockState(), this.getBlockState(), 3);
+    }
+
+    @Override
+    public void clearContent() {
+        this.items.clear();
+    }
+
+    public void dowse() {
+        if (this.level != null) {
+            this.markUpdated();
+        }
+    }
+
+    @Override
+    protected void applyImplicitComponents(BlockEntity.DataComponentInput pComponentInput) {
+        super.applyImplicitComponents(pComponentInput);
+        pComponentInput.getOrDefault(DataComponents.CONTAINER, ItemContainerContents.EMPTY).copyInto(this.getItems());
+    }
+
+    @Override
+    protected void collectImplicitComponents(DataComponentMap.Builder pComponents) {
+        super.collectImplicitComponents(pComponents);
+        pComponents.set(DataComponents.CONTAINER, ItemContainerContents.fromItems(this.getItems()));
+    }
+
+    @Override
+    public void removeComponentsFromTag(CompoundTag pTag) {
+        pTag.remove("Items");
+    }
+    
+    
+    ///
+
+
+    public boolean placeFuel(@Nullable LivingEntity pEntity, ItemStack pFood) {
+        int fuelAdded = AbstractFurnaceBlockEntity.getFuel().getOrDefault(pFood.getItem(), 0);
+        if (pFood.getItem() != Items.LAVA_BUCKET && this.fuelTime + fuelAdded <= 3000) {
+        	this.fuelTime += fuelAdded;
+        	pFood.shrink(1);
+        	this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.getBlockPos(), GameEvent.Context.of(pEntity, this.getBlockState()));
+            this.markUpdated();
+        	return true;
+        }
+
+        return false;
+    }
+}
